4bfa44f23ab4098bfd2bbc52709772bab53a4c07
Ivan Pereira <navi1921@gmail.com>
2019-10-03 20:29:43 -0300

    Update train_imagenet.py

--- train_imagenet.py
+++ train_imagenet.py
@@ -250,7 +250,68 @@ def train(train_queue, model, criterion, optimizer):
 
     return top1.avg, objs.avg
 
+  objs = utils.AvgrageMeter()
+  top1 = utils.AvgrageMeter()
+  top5 = utils.AvgrageMeter()
+  model.module.eval()
+  preds = np.asarray([])
+  targets = np.asarray([])
 
+  for step, (input, target) in enumerate(valid_queue):
+    #input = input.cuda()
+    #target = target.cuda(non_blocking=True)
+    input = Variable(input, volatile=True).cuda()
+    target = Variable(target, volatile=True).cuda(async=True)
+    logits = model(input)
+    loss = criterion(logits, target)
+
+    prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
+    n = input.size(0)
+    #objs.update(loss.data[0], n)
+    #top1.update(prec1.data[0], n)
+    #top5.update(prec5.data[0], n)
+    objs.update(loss.item(), n)
+    top1.update(prec1.item(), n)
+    top5.update(prec5.item(), n)
+    
+    #minha alteracao
+    output = logits
+    topk = (1,3)
+    maxk = max(topk)
+    batch_size = target.size(0)
+    _, predicted = torch.max(output.data, 1)
+    #minha alteracao
+    preds = np.concatenate((preds,predicted.cpu().numpy().ravel()))
+    #targets = np.concatenate((targets,target.cpu().numpy().ravel()))
+    targets = np.concatenate((targets,target.data.cpu().numpy().ravel()))
+
+
+    if step % args.report_freq == 0:
+      logging.info('valid %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
+  
+  print(preds.shape)
+  print(targets.shape)
+  print('np.unique(targets):',np.unique(targets))
+  print('np.unique(preds): ',np.unique(preds))
+  from sklearn.metrics import classification_report
+  from sklearn.metrics import accuracy_score
+  print(accuracy_score(targets, preds))
+  cr = classification_report(targets, preds,output_dict= True)
+  a1,a2,a3 = cr['macro avg']['f1-score'] ,cr['macro avg']['precision'],cr['macro avg']['recall'] 
+  topover = (a1+a2+a3)/3 
+  print(classification_report(targets, preds))
+  from sklearn.metrics import balanced_accuracy_score
+  from sklearn.metrics import accuracy_score
+  print(balanced_accuracy_score(targets, preds))
+  print(accuracy_score(targets, preds))
+  from sklearn.metrics import confusion_matrix
+  matrix = confusion_matrix(targets, preds)
+  print(matrix.diagonal()/matrix.sum(axis=1))
+  print(matrix)
+
+  return top1.avg,0, objs.avg
+
+"""
 def infer(valid_queue, model, criterion):
     objs = utils.AvgrageMeter()
     top1 = utils.AvgrageMeter()
@@ -282,6 +343,6 @@ def infer(valid_queue, model, criterion):
 
     return top1.avg, top5.avg, objs.avg
 
-
+"""
 if __name__ == '__main__':
     main() 
